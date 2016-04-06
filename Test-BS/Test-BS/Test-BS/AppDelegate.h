565fa25d86bb360e9f91862c559bdb616598a00a
Linkinzoo <ztzhengbc@163.com>
2016-04-06 22:48:49 +0800

    配置图标和启动图片。

---
+++ Test-BS/Test-BS/Test-BS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test-BS
+//
+//  Created by 郑博辰 on 16/4/6.
+//  Copyright © 2016年 com.link. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test-BS/Test-BS/Test-BS/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Test-BS
+//
+//  Created by 郑博辰 on 16/4/6.
+//  Copyright © 2016年 com.link. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
